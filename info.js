b4c393adb1de76b91e92d0432ed1045d0c435055
jan <57322982+mvte@users.noreply.github.com>
2023-02-16 19:28:32 -0500

    added more update methods

--- info.js
+++ info.js
@@ -1,9 +1,20 @@
 class Info {
-    static update(gen, name) {
+
+    static updateInfo(gen, name) {
         const generation = document.getElementById("generation");
         const bestFitness = document.getElementById("bestFitness");
 
         generation.innerHTML = gen;
         bestFitness.innerHTML = name;
+    }
+
+    static updateStatus(status) {
+        const statusBox = document.getElementById("status");
+        statusBox.innerHTML = status;
+        if(status == "Complete") statusBox.style.color = "green";
+        else if(status == "In Progress") { 
+            statusBox.style.color = "orange";
+            statusBox.innerHTML += "<br><button> Stop </button>";
+        }
     }
 }
